a4e8f2c4c2a1808f019c1f3694a346247a4f1937
nik-ex <150937524+nik-ex@users.noreply.github.com>
2023-12-17 22:59:44 +0400

    Update 1 task.cs

--- 1 task.cs
+++ 1 task.cs
@@ -1,1 +1,25 @@
+// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
 
+int firstNumber = ReadNumber("Введите начальное число: ");
+int lastNumber = ReadNumber("Введите конечное число: ");
+SumNumber(firstNumber, lastNumber);
+
+int ReadNumber(string userText)
+{
+    Console.Write(userText);
+    int numberUser = Convert.ToInt32(Console.ReadLine());
+    return numberUser;
+}
+
+void SumNumber(int numStart, int numEnd)
+{
+   if (numStart > numEnd) return;
+    {
+        Console.Write(numStart + " ");
+        numStart++;
+        SumNumber(numStart++, numEnd);
+    }
+} 
